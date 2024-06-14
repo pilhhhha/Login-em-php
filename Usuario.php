02164734148ff35f9a81b339e52e7c8d78bc7ac8
Marcelo Junior <pilhhhha@hotmail.com>
2024-06-14 07:20:09 -0300

    alguns comentarios para estudar melhor e ler melhor o codigo.

--- Usuario.php
+++ Usuario.php
@@ -1,6 +1,7 @@
 <?php
 
-class Usuario {
+class Usuario 
+{
     private $conn;
 
     public function __construct($conn) {
@@ -34,12 +35,16 @@ class Usuario {
             return "Erro na preparação da consulta: " . $this->conn->error;
         }
 
+        //bind_param function do php para bindar os parametros que estão em ? o primeiro parametro voce define qual TIPO ele é.
         $query->bind_param("s", $nome);
         $query->execute();
+        //store_result para armanezar o resultado da execução feita pela query
         $query->store_result();
 
         if ($query->num_rows > 0) {
+            //Se alguma linha for encontrada, bind_result estou pedindo para que ele binda o valor da $senhaHash ao campo senha da query
             $query->bind_result($senhaHash);
+            //Usa $query->fetch() para recuperar o valor do campo senha e armazená-lo em $senhaHash
             $query->fetch();
 
             if (!empty($senhaHash) && password_verify($senha, $senhaHash)) {
